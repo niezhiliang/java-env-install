6d5252e706304f43d2b814e2a835a84faf8e0462
苏雨丶 <33079130+niezhiliang@users.noreply.github.com>
2018-04-26 18:35:42 +0800

    Update java_install.sh

--- java_install.sh
+++ java_install.sh
@@ -6,7 +6,11 @@ yum -y install wget
 
 yum install glibc.i686 -y
 
-echo 'begin install Jdk1.8....'
+echo '***************************************'
+echo '*                                     *'
+echo '*      Jdk1.8 install ...             *'
+echo '*                                     *'
+echo '***************************************'
 
 wget -O jdk-8u144-linux-i586.tar.gz http://om40sen9v.bkt.clouddn.com/14ca4f70f43e41f4a593fee6e56a1541.gz
 
@@ -16,7 +20,11 @@ rm -f -r jdk-8u144-linux-i586.tar.gz
 
 mv  jdk1.8.0_144  /java/jdk1.8
 
-echo 'begin download Maven ....'
+echo '***************************************'
+echo '*                                     *'
+echo '*      Maven3.5 install ...             *'
+echo '*                                     *'
+echo '***************************************'
 
 wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
 
@@ -41,3 +49,21 @@ echo 'export PATH=${MAVEM_HOME}/bin:$PATH' >> /etc/profile
 
 
 source /etc/profile
+
+echo '***************************************'
+echo '*                                     *'
+echo '*      Tomcat8.5 install ...             *'
+echo '*                                     *'
+echo '***************************************'
+
+wget http://mirrors.shu.edu.cn/apache/tomcat/tomcat-8/v8.5.30/bin/apache-tomcat-8.5.30.tar.gz
+
+tar -zxvf apache-tomcat-8.5.30.tar.gz  
+
+rm -f -r apache-tomcat-8.5.30.tar.gz 
+
+mv  apache-tomcat-8.5.30 /java/apache-tomcat-8.5
+
+cd /java/apache-tomcat-8.5/bin/
+
+./startup.sh
