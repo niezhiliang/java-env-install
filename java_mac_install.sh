79ec40ddd8986336eec2284be12146ce03d88a53
苏雨丶 <33079130+niezhiliang@users.noreply.github.com>
2018-05-07 17:43:42 +0800

    Create java_mac_install.sh

---
+++ java_mac_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd ~
+
+cd Public
+
+mkdir java_env
+
+cd java_env 
+
+curl -O http://mirrors.hust.edu.cn/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
+
+tar -xzvf apache-maven-3.5.3-bin.tar.gz
+
+rm -f -r  apache-maven-3.5.3-bin.tar.gz
+
+cd ~
+
+if [ ! -e .bash_profile  ]; then
+
+
+	
+else 
+	touch .bash_profile
+
+fi
+
+cd Public/java_env/apache-maven-3.5.3
+
+path=`pwd`
+
+cd ~
+
+echo 'export MAVEM_HOME=${path}’ >> .bash_profile
+
+echo 'export PATH=${MAVEM_HOME}/bin:$PATH' >> .bash_profile
+
+source .bash_profile
+
+mvn -v
+
+
+
