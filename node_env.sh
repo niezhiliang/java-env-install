0837a393709409be55d42fb166986b5492c335eb
苏雨丶 <33079130+niezhiliang@users.noreply.github.com>
2018-07-22 19:17:38 +0800

    Update node_env.sh

--- node_env.sh
+++ node_env.sh
@@ -8,12 +8,16 @@ rm -f -r node-v8.11.3-linux-x64.tar.xz
 
 mv node-v8.11.3-linux-x64 nodejs
 
+#配置环境变量
 echo 'export NODE_HOME=/usr/local/nodejs' >> /etc/profile
 
 echo 'export PATH=$NODE_HOME/bin:$PATH' >> /etc/profile
 
 source /etc/profile
 
+#安装淘宝镜像
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+ 
 node -v
 
 npm -v
