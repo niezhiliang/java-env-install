ad9ad4f58a2515f7a0a17fdb0e16f2acc7bb0ab0
苏雨丶 <33079130+niezhiliang@users.noreply.github.com>
2018-07-25 10:21:13 +0800

    Update node_env.sh

--- node_env.sh
+++ node_env.sh
@@ -17,6 +17,10 @@ source /etc/profile
 
 #安装淘宝镜像
 npm install -g cnpm --registry=https://registry.npm.taobao.org
+
+npm install pm2 -g
+#配置pm2全局变量
+ln -s /usr/local/src/node-v8.9.0-linux-x64/bin/pm2  /usr/local/bin/pm2
  
 node -v
 
