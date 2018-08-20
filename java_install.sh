93bf5a758df9cc6cba42e7f4e367dfa21862cacf
苏雨丶 <33079130+niezhiliang@users.noreply.github.com>
2018-08-20 14:01:04 +0800

    Update java_install.sh

--- java_install.sh
+++ java_install.sh
@@ -26,13 +26,13 @@ echo '$      Maven3.5 install ...           $'
 echo '$                                     $'
 echo '======================================='
 
-wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
+wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
 
-tar -xzvf apache-maven-3.5.3-bin.tar.gz
+tar -xzvf apache-maven-3.5.4-bin.tar.gz
 
-rm -f -r  apache-maven-3.5.3-bin.tar.gz
+rm -f -r  apache-maven-3.5.4-bin.tar.gz
 
-mv  apache-maven-3.5.3 /java/apache-maven-3.5.3
+mv  apache-maven-3.5.4 /java/apache-maven-3.5.4
 
 #yum 安装
 # sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
@@ -48,15 +48,15 @@ echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> /etc/profile
 
 echo 'export PATH=${JAVA_HOME}/bin:$PATH' >> /etc/profile
 
-echo 'export MAVEM_HOME=/java/apache-maven-3.5.3' >> /etc/profile
+echo 'export MAVEM_HOME=/java/apache-maven-3.5.5' >> /etc/profile
 
 echo 'export PATH=${MAVEM_HOME}/bin:$PATH' >> /etc/profile
 
-if [ -e "/java/apache-maven-3.5.3/conf/settings.xml" ]; then
+if [ -e "/java/apache-maven-3.5.4/conf/settings.xml" ]; then
 
-  rm -f -r  /java/apache-maven-3.5.3/conf/settings.xml
+  rm -f -r  /java/apache-maven-3.5.4/conf/settings.xml
   
-  mv settings.xml /java/apache-maven-3.5.3/conf/
+  mv settings.xml /java/apache-maven-3.5.4/conf/
 fi
 
 
